ef9b084ac17634108c23d0736a5a0ab1da1e82d4
KMitzner9 <KMitzner9@gmail.com>
2020-09-29 16:12:17 -0700

    add javascript folder

---
+++ Javascript/Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+
+
+document.write("This is an official ho alert. Please stay inside." + " \"ConCATenate\", you say? "); // concatenating two strings
+
+var Z = "My cute kitten told me to \"Fuck the fuck right off.\" when I tried to pet her today.";     // Using backslashes to preserve quotation marks
+
+document.write(Z);                                                                                   // Printing a variable
+
+var A = "HO ALERT!!";                                                                                // Assigning a string to a variable
+
+window.alert(A);                                                                                     // printing a string variable in an alert box
+
+document.write(A + Z);                                                                               // printing a string using a function
+
+var Waterbender = "Katara", Airbender = "Aang", Earthbender = "Toph", Firebender = "Prince Zuko";    // assigning multiple variables in one line
+var Waterbender = Waterbender.fontcolor("blue");                                                     // assigning font colors to these variables
+var Airbender = Airbender.fontcolor("yellow");
+var Earthbender = Earthbender.fontcolor("green");
+var Firebender = Firebender.fontcolor("red");
+document.write(Earthbender + Airbender + Firebender + Waterbender);                                   // print all variables
+
+document.write(8 * 3);                                                                                // print a function
