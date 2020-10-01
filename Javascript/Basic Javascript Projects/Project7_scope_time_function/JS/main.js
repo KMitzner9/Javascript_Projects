cb71a6613f59911d65a6ea9db7d353f3e9e94d70
KMitzner9 <KMitzner9@gmail.com>
2020-10-01 15:27:11 -0700

    JS7

--- Javascript/Basic Javascript Projects/Project7_scope_time_function/JS/main.js
+++ Javascript/Basic Javascript Projects/Project7_scope_time_function/JS/main.js
@@ -1,3 +1,5 @@
+//set global variable and use it in two functions
+
 var X = 20;
 function Multiply_numbers_1() {
     document.write(2 * X + "<br>");
@@ -6,13 +8,19 @@ function Multiply_numbers_2() {
     document.write(X * 3 + "<br>");
 }
 
+//prints the answer to the broswer
+
 Multiply_numbers_1();
 Multiply_numbers_2();
+
+//set local variable and use in function
 
 function Add_numbers_1() {
     var Y = 10;
     document.write(X + Y + "<br>");
 }
+
+//create function that uses a local variable from another function to view error in console
 
 function Add_numbers_2() {
     document.write(X + Y);
@@ -20,6 +28,8 @@ function Add_numbers_2() {
 
 Add_numbers_1();
 Add_numbers_2();
+
+// use the Date().getHours() function to display different messages based on time of day
 
 function get_Date() {
     if (new Date().getHours() < 13) {
@@ -30,6 +40,8 @@ function get_Date() {
     }
 }
 
+//create my own conditional statement 
+
 function clap_hands() {
     var Happy = 5 > 3;
     var Know_it = 9 > 8;
@@ -38,12 +50,31 @@ function clap_hands() {
     }
 }
 
+//create function that takes user input and uses an if and else statement to display different messages
+
 function circus_function() {
     var Answer = document.getElementById("answer").value;
-    if (Answer = "yes") {
+    if (Answer == "yes") {
         document.getElementById("circus").innerHTML = "Awesome!!";
     }
     else {
         document.getElementById("circus").innerHTML = "What are you doing here!?";
     }
+}
+
+//create function that uses else if statement to display different messages based on time
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
