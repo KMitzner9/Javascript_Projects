abdfb9fd280f84a6ec9d166094dbbcd03e9776e2
KMitzner9 <KMitzner9@gmail.com>
2020-12-18 11:39:38 -0800

    portfolio site update

--- Javascript/Portfolio_Website/send.php
+++
@@ -1,8 +0,0 @@
-You typed in: <?php print $_POST['fullname'];?>
-
-<?php
-	$to = "KMitzner9@gmail.com";
-	$subject = "Email from my website";
-	$body = "the user typed in ...";
-	mail($to, $subject, $body);
-?>
